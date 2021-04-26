0dd9befe3e0fcfc712834e21cf87a346636d17c7
Giuditta <giudittaparolini@gmail.com>
2021-04-26 21:02:17 +0200

    pipeline project

--- slackbot_bvg/config.py
+++ slackbot_bvg/config.py
@@ -6,4 +6,4 @@ Created on Mon Apr 19 13:09:41 2021
 @author: giudittaparolini
 """
 
-WEBHOOK_URL = "https://hooks.slack.com/services/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"
+WEBHOOK_URL = "https://hooks.slack.com/services/XXXX"
